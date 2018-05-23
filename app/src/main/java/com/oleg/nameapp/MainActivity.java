612e9057769fb1df6dade4e98315b4e8499b1868
oleguka <olrgolrg@yandex.ru>
2018-05-23 20:19:25 +0500

    New
    cool

---
+++ app/src/main/java/com/oleg/nameapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.oleg.nameapp;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("Title")
+                .setMessage("Message")
+                .setCancelable(true)
+                .setIcon(R.drawable.ic_radio)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "You select Cancel ", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "You select Yes", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        Toast.makeText(getApplicationContext(), "Не делай так! :(", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+    }
+}
